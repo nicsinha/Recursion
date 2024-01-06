96350ca7c1859fd2654bcab7436a19d87aa4660b
nicsinha <sinhanikhil097@gmail.com>
2024-01-06 13:33:36 +0530

    Code for Printing all the subsequence

---
+++ printAllSubsequence.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSeq(int a[],int i,int n, vector<int> ans)
+{
+    if(i == n)
+    {
+        for(int k=0;k<ans.size();k++)
+            cout<<ans[k] <<" ";
+        cout<<endl;
+        return;
+    }
+    ans.push_back(a[i]);
+    printSeq(a,i+1,n,ans);
+    ans.pop_back();
+    printSeq(a,i+1,n,ans);
+}
+int main()
+{
+    int a[] = {3,2,1};
+    vector<int> ans;
+    printSeq(a,0,3,ans);
+    return 0;
+}
